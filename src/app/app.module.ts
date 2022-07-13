325fb68a9ed8ea3289dd4085ddf614b2903aa848
Jabin <israt.jabin@capgemini.com>
2022-07-13 16:23:08 +0530

    removed @angular/elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { Injector, NgModule } from '@angular/core';
-import { createCustomElement } from '@angular/elements'
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [],
-  entryComponents: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: []
 })
 export class AppModule { 
-  constructor(private injector: Injector) {
-    const AppElement = createCustomElement(AppComponent, { injector: this.injector });
-    window.customElements.define('app-root', AppElement);
-  }
-
-  // Empty bootstrap call allow our custom elements to bootstrap them selves
-  // Required by Angular since we did not specify which component to bootstrap in @NgModule
-  ngDoBootstrap() {
-  }
 }
 
